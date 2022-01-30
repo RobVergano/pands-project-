955fb8c2a96f560525c44f7273196a1c23881a03
RobVergano <10097802+RobVergano@users.noreply.github.com>
2022-01-30 19:07:27 +0000

    Week 2

---
+++ Week 2/nameAndAge.py
@@ -0,0 +1,7 @@
+#nameAndAge.py
+#this program reads name and age
+#author: Roberto Vergano
+
+name = input ("Enter your name:")
+years = input ("Enter your age:")
+print ("Hello " + name +"," + "\tYour age is " + years + "\nNice to meet you")
